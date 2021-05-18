787fa6c1de68211cd6f5e0730a4e6130d2a4ed2b
Marwa  Awwad <marwaawwad10@gmail.com>
2021-05-18 22:46:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+/*
+function mapToNegativize(array){return array.map(x => x * (-1));}
+function mapToNoChange(array){return array.map(x => x );}
+function mapToDouble(array){return array.map(x => x * 2);}
+function mapToSquare(array){return array.map(x => x*x);}
+function functionUsingCallback(en,array){
+  en(array)
+  }
+*/
+//functionUsingCallback(mapToNegativize,[1, 2, 3, -9]);
+
+function map(a,f){
+ function(a){ return a * (-1) },
+ function(a){ return a  },
+ function(a){ return a * 2 },
+ function(a){ return a * a }
+ }
+map([1, 2, 3, -9], function(a){ return a * a });
