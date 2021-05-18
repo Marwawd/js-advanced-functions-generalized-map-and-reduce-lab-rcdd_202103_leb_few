f0f0406bc83b39b93156c7d60269991ec496af62
Marwa  Awwad <marwaawwad10@gmail.com>
2021-05-18 22:58:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 // Add your functions here
-/*
+
 function mapToNegativize(array){return array.map(x => x * (-1));}
 function mapToNoChange(array){return array.map(x => x );}
 function mapToDouble(array){return array.map(x => x * 2);}
 function mapToSquare(array){return array.map(x => x*x);}
-function functionUsingCallback(en,array){
-  en(array)
+function map( mapToNegativize,
+mapToNoChange,
+mapToDouble,
+mapToSquare,array){
+mapToNegativize(array)
+mapToNoChange(array)
+mapToDouble(array)
+mapToSquare(array)
   }
-*/
+
 //functionUsingCallback(mapToNegativize,[1, 2, 3, -9]);
 
 
 
 
-function sayHello(name="") {
-  console.log(`Hello${name}`)
-}
-
-let sayHola = function(name="") {
-  console.log(`Hola${name}`)
-}
-
-functionUsingCallback(sayHello, sayHola, function(name="") {
-  console.log(`Ni Hao${name}`)
-}, " Gary")
-
-function functionUsingCallback(en, es, zh, name){
-  en(name)
-  es(name)
-  zh(name)
-}
+// function sayHello(name="") {
+//   console.log(`Hello${name}`)
+// }
+// 
+// let sayHola = function(name="") {
+//   console.log(`Hola${name}`)
+// }
+// 
+// functionUsingCallback(sayHello, sayHola, function(name="") {
+//   console.log(`Ni Hao${name}`)
+// }, " Gary")
+// 
+// function functionUsingCallback(en, es, zh, name){
+//   en(name)
+//   es(name)
+//   zh(name)
+// }
